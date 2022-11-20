7a868439c44b5fbc56e5c61cd626cfd5e40d5384
Hediye Yildirim <hediyeyldrm0@gmail.com>
2022-11-20 18:04:28 +0300

    routes added

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { BrandAddComponent } from './components/brand-add/brand-add.component';
+import { CarAddComponent } from './components/car-add/car-add.component';
+import { CarDetailComponent } from './components/car-detail/car-detail.component';
+import { BrandsComponent } from './components/brands/brands.component';
+import { CarListComponent } from './components/car-list/car-list.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', component: CarListComponent },
+  { path: 'car-list/:state', component: CarListComponent },
+  { path: 'brands/:id', component: CarListComponent },
+  { path: 'car-detail/:id', component: CarDetailComponent },
+  { path: 'car-add', component: CarAddComponent },
+  { path: 'brand-add', component: BrandAddComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
