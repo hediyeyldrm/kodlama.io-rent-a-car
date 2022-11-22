a3e28b0d4dcb7ce9ed205d9be437bb21fdf6d220
Hediye Yildirim <hediyeyldrm0@gmail.com>
2022-11-22 17:04:37 +0300

    colors component added

---
+++ src/app/components/colors/colors.component.ts
@@ -0,0 +1,32 @@
+import { ColorsModel } from './../../models/ColorsModel';
+import { ColorsService } from './../../services/colors.service';
+import { CarListModel } from './../../models/CarListModel';
+import { ActivatedRoute } from '@angular/router';
+import { CarListService } from './../../services/car-list.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-colors',
+  templateUrl: './colors.component.html',
+  styleUrls: ['./colors.component.css'],
+})
+export class ColorsComponent implements OnInit {
+  colors: ColorsModel[] = [];
+  colorsName!: string;
+  constructor(
+    private colorsService: ColorsService,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.getColors();
+  }
+
+  getColors() {
+    this.colorsService.getColors().subscribe((data) => (this.colors = data));
+  }
+
+  selectColor(data: any) {
+    this.colorsName = data.colorName;
+  }
+}
